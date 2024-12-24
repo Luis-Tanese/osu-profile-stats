72df63b8ecbc9d5913ff51735dfbbc79197a4da7
Luis Tanese <luistanese@gmail.com>
2024-12-24 01:02:19 -0300

    Update utils.js

--- middleware/utils.js
+++ middleware/utils.js
@@ -17,7 +17,7 @@ const getBackground = (background, hex = null, svgWidth, svgHeight) => {
         const bgTypes = ["default", "bg1", "bg2", "bg3"];
         const selectedBg = bgTypes.includes(background) ? background : "default";
 
-        return `<image href="/assets/images/backgrounds/${selectedBg}.jpg" x="0" y="0" width="${svgWidth}" height="${svgHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-rounded)" />`;
+        return `<image href="https://osu-profile-stats.vercel.app/assets/images/backgrounds/${selectedBg}.jpg" x="0" y="0" width="${svgWidth}" height="${svgHeight}" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip-rounded)" />`;
     }
 };
 
